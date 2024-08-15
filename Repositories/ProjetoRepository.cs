c4441191ae1446e88f063f548ab914fad5525a48
wiresmoura <wiresmoura3@gmail.com>
2024-08-15 17:52:30 -0300

    Parte 2- Atividade: Criação e teste do CRUD da API com uso do softwre de Teste Insomnia

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -3,6 +3,7 @@ using Exo.WebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 // ProjetoRepository.cs Faz parte de um padrão de projeto que utilizamos no desenvolvimento 
@@ -22,5 +23,34 @@ namespace Exo.WebApi.Repositories
         {
             return _context.Projetos.ToList();
         }
+        /////////
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+        public Projeto BuscarporId(int id)
+        {
+            return _context.Projetos.Find(id);
+        }
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            if (projetoBuscado != null)
+            {
+                projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+                projetoBuscado.Area = projeto.Area;
+                projetoBuscado.Status = projeto.Status;
+            }
+            _context.Projetos.Update(projetoBuscado);
+            _context.SaveChanges();
+        }
+        public void Deletar(int id)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projetoBuscado);
+            _context.SaveChanges();
+        }
+
     }
 }
